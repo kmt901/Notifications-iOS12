a25bfca8adc4ba5ccd3bb68a4c738e45b90054dd
Kaya Thomas <kayathom@gmail.com>
2018-08-26 23:03:25 -0600

    remove force unwraps

--- NotificationContentExt/NotificationViewController.swift
+++ NotificationContentExt/NotificationViewController.swift
@@ -29,16 +29,14 @@ class NotificationViewController: UIViewController, UNNotificationContentExtensi
         self.episodeNameLabel.text = notification.request.content.body
 
         let imgAttachment = notification.request.content.attachments[0]
-        let imageData = NSData(contentsOf: imgAttachment.url)
-        let image = UIImage(data: imageData! as Data)
-
         let buttonNormalStateAtt = notification.request.content.attachments[1]
-        let buttonNormalStateImgData = NSData(contentsOf: buttonNormalStateAtt.url)
-        let buttonNormalStateImg = UIImage(data: buttonNormalStateImgData! as Data)?.withRenderingMode(.alwaysOriginal)
-
         let buttonHighlightStateAtt = notification.request.content.attachments[2]
-        let buttonHighlightStateImgData = NSData(contentsOf: buttonHighlightStateAtt.url)
-        let buttonHighlightStateImg = UIImage(data: buttonHighlightStateImgData! as Data)?.withRenderingMode(.alwaysOriginal)
+
+        guard let imageData = NSData(contentsOf: imgAttachment.url), let buttonNormalStateImgData = NSData(contentsOf: buttonNormalStateAtt.url), let buttonHighlightStateImgData = NSData(contentsOf: buttonHighlightStateAtt.url) else { return }
+
+        let image = UIImage(data: imageData as Data)
+        let buttonNormalStateImg = UIImage(data: buttonNormalStateImgData as Data)?.withRenderingMode(.alwaysOriginal)
+        let buttonHighlightStateImg = UIImage(data: buttonHighlightStateImgData as Data)?.withRenderingMode(.alwaysOriginal)
 
         imageView.image = image
         likeButton.setImage(buttonNormalStateImg, for: .normal)
@@ -51,31 +49,29 @@ class NotificationViewController: UIViewController, UNNotificationContentExtensi
 
     func didReceive(_ response: UNNotificationResponse, completionHandler completion:
         (UNNotificationContentExtensionResponseOption) -> Void) {
+        guard let currentActions = extensionContext?.notificationActions else { return }
+
         if response.actionIdentifier == "play-action" {
             let pauseAction = UNNotificationAction(identifier: "pause-action", title: "Pause", options: [])
-            let currentActions = extensionContext?.notificationActions
-            let otherAction = currentActions![1]
+            let otherAction = currentActions[1]
             let newActions = [pauseAction, otherAction]
             extensionContext?.notificationActions = newActions
 
         } else if response.actionIdentifier == "queue-action" {
             let removeAction = UNNotificationAction(identifier: "remove-action", title: "Remove from Queue", options: [])
-            let currentActions = extensionContext?.notificationActions
-            let otherAction = currentActions![0]
+            let otherAction = currentActions[0]
             let newActions = [otherAction, removeAction]
             extensionContext?.notificationActions = newActions
 
         }  else if response.actionIdentifier == "pause-action" {
             let playAction = UNNotificationAction(identifier: "play-action", title: "Play", options: [])
-            let currentActions = extensionContext?.notificationActions
-            let otherAction = currentActions![1]
+            let otherAction = currentActions[1]
             let newActions = [playAction, otherAction]
             extensionContext?.notificationActions = newActions
 
         } else if response.actionIdentifier == "remove-action" {
             let queueAction = UNNotificationAction(identifier: "queue-action", title: "Queue Next", options: [])
-            let currentActions = extensionContext?.notificationActions
-            let otherAction = currentActions![0]
+            let otherAction = currentActions[0]
             let newActions = [otherAction, queueAction]
             extensionContext?.notificationActions = newActions
         }
